86f9032c54dc81042573724c91c34e0b18732f53
clair3st <clairejgatenby@gmail.com>
2025-06-03 16:15:59 -0700

    adding comments to event listner

--- contentScript.js
+++ contentScript.js
@@ -1,55 +1,69 @@
+import { getTime } from "./utils.js";
 (() => {
-    let youtubeLeftControls, youtubePlayer;
+    let youtubeLeftControls, youtubePlayer; //access youtube player and youtube controls
     let currentVideo = "";
     let currentVideoBookmarks = [];
 
+    // Send a response back to message sent from background.js
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const { type, value, videoId } = obj;
 
         if (type === "NEW") {
             currentVideo = videoId;
-            newVideoLoaded();
+            newVideoLoaded(); //handle any actions with new video
         }
     });
 
-    const newVideoLoaded = () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        console.log(bookmarkBtnExists);
+    //fetch bookmarks from chrome storage asynchronously
+    const fetchBookmarks = () => {
+        return new Promise((resolve) => {
+            chrome.storage.sync.get([currentVideo], (obj) => {
+                resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+            });
+        });
+    };
 
-        if (!bookmarkBtnExists) {
+    //create a bookmark button for the current video
+
+    const newVideoLoaded = async () => {
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+        currentVideoBookmarks = await fetchBookmarks();
+
+        if (!bookmarkBtnExists) { //if bookmark button does not exist, create it for any youtube video
             const bookmarkBtn = document.createElement("img");
 
+            //Create the new button
             bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
             bookmarkBtn.className = "ytp-button " + "bookmark-btn";
             bookmarkBtn.title = "Click to bookmark current timestamp";
 
+            //Get the youtube player and youtube controls
             youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
             youtubePlayer = document.getElementsByClassName("video-stream")[0];
-            
+
+            //Append the new button to the youtube player and add event listener
             youtubeLeftControls.append(bookmarkBtn);
             bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
         }
     }
 
-    const addNewBookmarkEventHandler = () => {
+    //Event handler bookmark button, grab video timestamp and create bookmark
+    const addNewBookmarkEventHandler = async () => {
         const currentTime = youtubePlayer.currentTime;
         const newBookmark = {
             time: currentTime,
             desc: "Bookmark at " + getTime(currentTime),
         };
-        console.log(newBookmark);
+        
+        currentVideoBookmarks.push(newBookmark) = await fetchBookmarks();
 
         chrome.storage.sync.set({
             [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
         });
     }
 
+    //Call anytime we hit youtube, will ensure it is called regardless of a youtube url change
     newVideoLoaded();
 })();
 
-const getTime = t => {
-    var date = new Date(0);
-    date.setSeconds(1);
 
-    return date.toISOString().substr(11, 0);
-}
