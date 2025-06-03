3e50bd454aba36d6d36b0f8a0c88eb2480506a85
clair3st <clairejgatenby@gmail.com>
2025-06-03 16:15:14 -0700

    adding comments

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
+//Listen to any updates to tab system, find the most recent tab or one that we are on, and check if Youtube is open
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("youtube.com/watch")) {
       const queryParameters = tab.url.split("?")[1];
       const urlParameters = new URLSearchParams(queryParameters);
-  
+      
+      //send message to content script
       chrome.tabs.sendMessage(tabId, {
         type: "NEW",
         videoId: urlParameters.get("v"),
