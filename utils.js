456e968c0404c09dc7aa96b787023e61d5bd243f
clair3st <clairejgatenby@gmail.com>
2025-06-03 17:39:57 -0700

    removing get time function

--- utils.js
+++ utils.js
@@ -1,14 +1,6 @@
 //Find active tab user is on, from Chrome documentation
 export async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true };
+    let queryOptions = { active: true, lastFocusedWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
-}
-
-//Helper function to get the time in the format of HH:MM:SS
-export const getTime = t => {
-    var date = new Date(0);
-    date.setSeconds(1);
-
-    return date.toISOString().substr(11, 0);
 }
