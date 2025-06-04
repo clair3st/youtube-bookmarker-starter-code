091579cfc9f918a6b390a0ace5ed0cab71cf99c9
clair3st <clairejgatenby@gmail.com>
2025-06-03 17:38:07 -0700

    adding fix to bug

--- background.js
+++ background.js
@@ -1,14 +1,27 @@
 //Listen to any updates to tab system, find the most recent tab or one that we are on, and check if Youtube is open
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
+
+
+  const sendMessageToContentScript = (tabId, url) => {
+    if (url && url.includes("youtube.com/watch")) {
+      const queryParameters = url.split("?")[1];
       const urlParameters = new URLSearchParams(queryParameters);
-      
-      //send message to content script
+  
       chrome.tabs.sendMessage(tabId, {
         type: "NEW",
         videoId: urlParameters.get("v"),
       });
     }
+  };
+  
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete') {
+      sendMessageToContentScript(tabId, tab.url);
+    }
+  });
+  
+  chrome.tabs.onActivated.addListener((activeInfo) => {
+    chrome.tabs.get(activeInfo.tabId, (tab) => {
+      sendMessageToContentScript(activeInfo.tabId, tab.url);
+    });
   });
   
